47018ee18537cbc6a1c3f41b2f76a0ab7f7a2ed7
klmin <haemmas6394@gmail.com>
2025-02-04 18:24:47 +0900

    feat : Add drivr-api module

---
+++ drive-api/src/main/java/com/tdrive/driveapi/DriveApiApplication.java
@@ -0,0 +1,13 @@
+package com.tdrive.driveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriveApiApplication.class, args);
+    }
+
+}
---
+++ drive-api/src/test/java/com/tdrive/driveapi/DriveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdrive.driveapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
