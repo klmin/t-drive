07a1002fa92af7682bc4536353bb697771396787
klmin <haemmas6394@gmail.com>
2025-02-13 14:22:14 +0900

    Add module aaplication

---
+++ drive-application/build.gradle.kts
@@ -0,0 +1,6 @@
+tasks.named("bootJar").configure { enabled = false }
+tasks.named("jar").configure { enabled = true }
+
+dependencies {
+
+}
---
+++ drive-application/src/test/java/com/tdrive/application/DriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdrive.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
